fd83c4b560b846f1b1d72353bee7be61841bb6b3
Brian Desplantes <heftygac@gmail.com>
2020-01-14 20:36:36 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,13 @@
-# code the #greeting method here!
+def greeting (name)
+  new_name = ""
+  i = 0
+
+  while (i < name.length)
+    if (name[i] != " ")
+      new_name += name[i]
+    end
+		i += 1
+  end
+
+  puts "Hello #{new_name}. It's nice to meet you."
+end
